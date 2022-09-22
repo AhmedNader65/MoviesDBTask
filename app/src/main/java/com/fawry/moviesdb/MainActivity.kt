b0d1551c83f8ce47a8edaab5767f10d45f996ef6
AhmedNader <ahmed.nader1994@gmail.com>
2022-09-22 17:15:14 +0200

    fixed hilt dependency

--- app/src/main/java/com/fawry/moviesdb/MainActivity.kt
+++ app/src/main/java/com/fawry/moviesdb/MainActivity.kt
@@ -3,7 +3,9 @@ package com.fawry.moviesdb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/fawry/moviesdb/data/cache/RoomCache.kt
+++ app/src/main/java/com/fawry/moviesdb/data/cache/RoomCache.kt
@@ -1,9 +1,13 @@
 package com.fawry.moviesdb.data.cache
 
+import com.fawry.moviesdb.data.cache.daos.MoviesDao
 import com.fawry.moviesdb.data.cache.model.CachedMovie
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class RoomCache : Cache {
+class RoomCache @Inject constructor(
+    private val moviesDao: MoviesDao,
+) : Cache {
     override fun getMovies(): Flow<List<CachedMovie>> {
         TODO("Not yet implemented")
     }
